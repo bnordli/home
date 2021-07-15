9c2831938ed6db2a7d214ee7ca65e2154721f686
Børge Nordli <bnordli@gmail.com>
2021-07-15 23:03:36 +0200

    Friendly output

--- raspberry/rfx_remote.sh
+++ raspberry/rfx_remote.sh
@@ -2,4 +2,7 @@
 # Install: "sudo apt-get install socat"
 # Run to connect remotely to RFXtrx433XL, on port 3007
 
+echo "Opening connection on $(ifconfig eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*'):3007"
+echo "Press CTRL+C or disconnect RFXmngr to exit."
+
 socat file:/dev/serial/by-id/usb-RFXCOM_RFXtrx433XL_DO5OOZ1D-if00-port0,raw,echo=0 tcp4-l:3007
